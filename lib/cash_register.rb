5db75e322a60997809608c7ca29cc41ba83422fd
Taishawn King <taishawnking1@yahoo.com>
2020-01-17 21:22:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,13 @@
 require"pry"
 class CashRegister
 attr_accessor :total, :discount
+items = []
 def initialize(discount=0)
 @total =0
 @discount = discount
   end
   def add_item(title,price,quantity = 1)
-    
+    items << title
   #binding.pry  
 end
 end
