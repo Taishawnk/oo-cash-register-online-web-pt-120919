dd0bff7bd33837c0b316e93a74d2b9c428e283e7
Taishawn King <taishawnking1@yahoo.com>
2020-01-17 22:22:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,21 +6,22 @@ def initialize(discount=0)
 @items = []
 @total =0
 @discount = discount
-  end
+end
   def add_item(title,price,quantity = 1)
     @total += price * quantity
     quantity.times do 
     @items << title 
   end
-  @last_amount = price*quantity
+
+@last_amount = price*quantity
 end
+
 def apply_discount 
 if @discount > 0 
 @total = @total-(@discount/100 * @total) #pemdas
 return"After the discount, the total comes to #{@total}."
 else 
 return "There is no discount to apply."
-
 end
 end
 end
