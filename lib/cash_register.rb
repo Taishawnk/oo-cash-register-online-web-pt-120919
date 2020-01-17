cb5c1c6e4ce923b196e2b9a129ca21b5d3aa3e04
Taishawn King <taishawnking1@yahoo.com>
2020-01-17 22:22:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ end
 
 def apply_discount 
 if @discount > 0 
-@total = @total-(@discount/100 * @total) #pemdas
+@total = @total-(@total * @discount/100) #pemdas
 return"After the discount, the total comes to #{@total}."
 else 
 return "There is no discount to apply."
