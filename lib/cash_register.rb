4f2cbc7738e00b947ccb6d37e5c16577c95cd43d
Taishawn King <taishawnking1@yahoo.com>
2020-01-17 21:47:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,13 +4,14 @@ attr_accessor :total, :discount
 @@items = []
 def initialize(discount=0)
 @total =0
-@discount = discount
+@discount = discount/100
   end
   def add_item(title,price,quantity = 1)
     @total += price * quantity
     @@items << title 
 end
 def apply_discount 
-  @discount % 100 = @total 
+total = (@discount * @total)-total #pemdas
+binding.pry
 end
 end
