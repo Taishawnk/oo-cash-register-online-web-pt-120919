dbc9a3ecbc8b05c1401d1511a6cb74b5d11774f2
Taishawn King <taishawnking1@yahoo.com>
2020-01-17 21:48:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ attr_accessor :total, :discount
 @@items = []
 def initialize(discount=0)
 @total =0
-@discount = discount/100
+@discount = discount
   end
   def add_item(title,price,quantity = 1)
     @total += price * quantity
