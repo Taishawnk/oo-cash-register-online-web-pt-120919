6ba9f6ce00468ead70b36365f40310888f900d29
Taishawn King <taishawnking1@yahoo.com>
2020-01-17 22:55:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,14 @@
 require"pry"
 class CashRegister
 attr_accessor :total, :discount,:last_amount
-def initialize(discount=0)
-@items = []
+def initialize(discount=0) #initialized the discount set to 0 
+@items = [] every time we initialize we have are empty array if empty 
 @total =0
 @discount = discount
 end
 
   def add_item(title,price,quantity = 1)
-    @total += price * quantity
+    @total += price * quantity # the total is now equaled to the price of the iteam times the quantity of the iteam 
     quantity.times do 
     @items << title 
 end
@@ -18,7 +18,7 @@ end
 
 def apply_discount 
 if @discount > 0 
-@total = @total-(@total * @discount/100) #pemdas
+@total = @total-(@total * @discount/100) #pemdas this is the formula for applaying a discount 
 return"After the discount, the total comes to $#{@total}."
 else 
 return "There is no discount to apply."
